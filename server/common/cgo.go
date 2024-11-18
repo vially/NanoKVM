6de24db414c2a50b631573a9c20009a3c7d35f75
Valentin Hăloiu <valentin.haloiu@gmail.com>
2024-11-18 20:13:18 +0000

    [server] Rip out kvm vision CGO implementation

--- server/common/cgo.go
+++ server/common/cgo.go
@@ -1,16 +1,9 @@
 package common
 
-/*
-	#cgo CFLAGS: -I../include
-	#cgo LDFLAGS: -L../dl_lib -lkvm
-	#include "kvm_vision.h"
-*/
-import "C"
 import (
 	"NanoKVM-Server/config"
 	"strings"
 	"sync"
-	"unsafe"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -31,12 +24,13 @@ func GetKvmVision() *KvmVision {
 		conf := config.GetInstance()
 		logLevel := strings.ToLower(conf.Logger.Level)
 
-		logEnable := C.uint8_t(0)
+		logEnable := false
 		if logLevel == "debug" {
-			logEnable = C.uint8_t(1)
+			logEnable = true
 		}
 
-		C.kvmv_init(logEnable)
+		log.Debugf("TODO(vially): initialize kvm vision %t", logEnable)
+
 		log.Debugf("kvm vision initialized")
 	})
 
@@ -47,26 +41,13 @@ func (k *KvmVision) ReadMjpeg(width uint16, height uint16, quality uint16) (data
 	k.mutex.Lock()
 	defer k.mutex.Unlock()
 
-	var (
-		kvmData  *C.uint8_t
-		dataSize C.uint32_t
-	)
+	log.Debugf("TODO(vially): implement `KvmVision.ReadMjpeg`")
 
-	result = int(C.kvmv_read_img(
-		C.uint16_t(width),
-		C.uint16_t(height),
-		C.uint8_t(0),
-		C.uint16_t(quality),
-		&kvmData,
-		&dataSize,
-	))
+	result = -1
 	if result < 0 {
 		log.Errorf("failed to read kvm image: %v", result)
 		return
 	}
-	defer C.free_kvmv_data(&kvmData)
-
-	data = C.GoBytes(unsafe.Pointer(kvmData), C.int(dataSize))
 
 	log.Debugf("read kvm image: %v", result)
 	return
@@ -76,73 +57,23 @@ func (k *KvmVision) ReadH264(width uint16, height uint16, bitRate uint16) (data 
 	k.mutex.Lock()
 	defer k.mutex.Unlock()
 
-	var (
-		kvmData  *C.uint8_t
-		dataSize C.uint32_t
-	)
+	log.Debugf("TODO(vially): implement `KvmVision.ReadH264`")
 
-	result = int(C.kvmv_read_img(
-		C.uint16_t(width),
-		C.uint16_t(height),
-		C.uint8_t(1),
-		C.uint16_t(bitRate),
-		&kvmData,
-		&dataSize,
-	))
+	result = -1
 	if result < 0 {
 		log.Errorf("failed to read kvm image: %v", result)
 		return
-	}
-	defer C.free_kvmv_data(&kvmData)
-
-	data = C.GoBytes(unsafe.Pointer(kvmData), C.int(dataSize))
-
-	if result == 3 {
-		sps, _ = k.ReadH264SPS()
-		pps, _ = k.ReadH264PPS()
 	}
 
 	log.Debugf("read kvm image: %v", result)
 	return
 }
 
-func (k *KvmVision) ReadH264SPS() ([]byte, int) {
-	var (
-		kvmData  *C.uint8_t
-		dataSize C.uint32_t
-	)
-
-	result := C.kvmv_get_sps_frame(&kvmData, &dataSize)
-	if result < 0 {
-		log.Errorf("failed to read sps: %v", result)
-		return nil, int(result)
-	}
-
-	data := C.GoBytes(unsafe.Pointer(kvmData), C.int(dataSize))
-
-	return data, int(result)
-}
-
-func (k *KvmVision) ReadH264PPS() ([]byte, int) {
-	var (
-		kvmData  *C.uint8_t
-		dataSize C.uint32_t
-	)
-
-	result := C.kvmv_get_pps_frame(&kvmData, &dataSize)
-	if result < 0 {
-		log.Errorf("failed to read pps: %v", result)
-		return nil, int(result)
-	}
-
-	data := C.GoBytes(unsafe.Pointer(kvmData), C.int(dataSize))
-	return data, int(result)
-}
-
 func (k *KvmVision) Close() {
 	k.mutex.Lock()
 	defer k.mutex.Unlock()
 
-	C.kvmv_deinit()
+	log.Debugf("TODO(vially): implement `KvmVision.Close`")
+
 	log.Debugf("stop kvm vision...")
 }
